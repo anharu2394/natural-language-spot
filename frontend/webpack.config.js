f980a68b700f931d2c6abadfec0ef1f7e539037c
安藤春香 <haruan2394@gmail.com>
2018-10-07 12:55:45 +0900

    Change: Change webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = { 
   mode: 'development',
+  devtool: "inline-source-map",
   entry: [path.resolve(__dirname, './src/app.ts')],
   output: {
     publicPath: '/',
@@ -23,11 +24,11 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         loader: 'ts-loader'
-      }   
+      },
     ]   
   },  
   resolve: {
-    extensions: ['.ts', '.tsx','.js'],
+    extensions: ['.ts', '.tsx','.js', '.jsx'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
