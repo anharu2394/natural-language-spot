f486a081a2b51cc4e87383400abcbd5836326f5d
安藤春香 <haruan2394@gmail.com>
2018-10-07 18:16:55 +0900

    Add: Add mounting app

---
+++ frontend/src/app.ts
@@ -0,0 +1,6 @@
+import { app } from "hyperapp"
+import { state, State } from "./state"
+import { actions, Actions } from "./actions"
+import view from "./view"
+
+app<State, Actions>(state, actions, view, document.body)
