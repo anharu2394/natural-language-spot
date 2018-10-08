8b5b256eab95a1e198b6cf125c2d763ffca51af1
安藤春香 <haruan2394@gmail.com>
2018-10-08 20:58:05 +0900

    Add: Add header icon and title

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,8 +1,21 @@
 import { h, Component } from "hyperapp"
+import styled from "./../picostyle"
+import Img from "./shared/image"
+import Columns from "./shared/columns"
+import Column from "./shared/column"
+import Title from "./shared/title"
+import Icon from "./../images/icon.png"
 
 const Header: Component = () => (
   <div>
-    Header
+    <Columns>
+      <Column is={2}>
+        <Img src={Icon} height="100px" />
+      </Column>
+      <Column>
+        <Title>第二外国語を応援するWeb</Title>
+      </Column>
+    </Columns>
   </div>
 )
 
